6e04543d772c38f9dc122bca074847e23a98d123
Harizo Rajaona <harizo.rajaona@gmail.com>
2023-06-26 19:06:54 +0200

    Add rust-analyzer in LSP config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,8 +1,19 @@
-local lsp = require('lsp-zero').preset({
-	name = 'minimal',
-	set_lsp_keymaps = true,
-	manage_nvim_cmp = true,
-	suggest_lsp_servers = false,
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({buffer = bufnr})
+end)
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+-- rust-analyzer
+require("lspconfig").rust_analyzer.setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+    cmd = {
+        "rustup", "run", "stable", "rust-analyzer"
+    }
 })
 
 lsp.setup()
